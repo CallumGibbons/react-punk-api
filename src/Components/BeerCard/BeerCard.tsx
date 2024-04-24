f5d21d639118f5ba450810dfdbc130e00b152791
callum <callumgibbons@icloud.com>
2024-04-24 23:29:11 +0530

    created beer card to be used for every index/id

---
+++ src/Components/BeerCard/BeerCard.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { Beer } from '../../assets/Data/types';
+
+interface BeerCardProps {
+    beer: Beer;
+  }
+
+function BeerCard({beer}: BeerCardProps) {
+  return (
+    <div className="beer-card">
+      <img src={beer.image_url} alt={beer.name} />
+      <h2>{beer.name}</h2>
+      <p>{beer.tagline}</p>
+      <p>{beer.description}</p>
+      <p>ABV: {beer.abv}%</p>
+    </div>
+  );
+}
+
+export default BeerCard
