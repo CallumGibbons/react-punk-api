8b9d8193de4d0e21f9a49c1d138ac71336ab32fa
callum <callumgibbons@icloud.com>
2024-04-24 20:20:33 +0530

    added preliminary placeholders before working on cards and containers, open to change.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import { useState } from 'react'
+import {Routes, Route, Link} from "react-router-dom"
+import { Beer } from './assets/Data/types'
+import beers from './assets/Data/beers'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchValue, setSearchValue] = useState<string>("");
+  const [filters, setFilters] = useState<string[]>([]);
+  
+  
+  
+  
+  
+  
+  
+  return (
+    <div>
+    <Nav/>
+    <beerCardContainer/>
+    </div>
+  )
 }
+
+
 export default App
